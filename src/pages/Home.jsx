dc180a2c7de025c0804c0c7c8169df7cb5ddca16
Jonh-ratana <ratanayorm787@gmail.com>
2025-09-09 13:15:09 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import New from "../components/New";
+import Slidebar from "../components/Slidebar";
+
+export default function Home(){
+    return(
+        <>
+           <div className="px-1 py-2  ">
+             <Slidebar/>
+             <New/>
+             សួស្តី
+           </div>
+        </>
+    )
+}
